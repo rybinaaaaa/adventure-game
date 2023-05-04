3b6c4081a177ff4071922c238aeac53c4ab3f123
rybinali <rybinaaaaa.a@gmail.com>
2023-05-04 23:13:23 +0200

    add potion

--- src/main/Controller/Controller.java
+++ src/main/Controller/Controller.java
@@ -1,10 +1,11 @@
 package main.Controller;
 
+import main.Model.Entity.Player;
 import main.Model.Levels.Levels;
 import main.Model.Levels.Map;
 import main.Model.Potion.Potion;
 import main.Model.Tiles.Tile;
-import main.Model.Entity.Player;
+
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,15 +17,15 @@ public class PlayerController {
 
     Levels levels;
     Map map;
-    private int jumpingHeight = 0;
-    private int timer = 10;
 
+    private boolean jumping = false;
+    private int jumpingHeight = 0;
     private boolean isCollisionTop = false,
             isCollisionBottom = false,
             isCollisionMove = false;
 
 
-    private void updateMoveState() {
+    private void updateCollision() {
         Tile top = map.getTile(player.getX(), player.getY());
         Tile bottom = map.getTile(player.getX() + player.getWidth() / 2, player.getY() + player.getHeight());
         Tile present = map.getTile(player.getX() + player.getWidth() / 2, player.getY() + 48);
@@ -33,7 +34,8 @@ public class PlayerController {
         isCollisionBottom = bottom.isCollision();
         isCollisionMove = next.isCollision();
         player.setHealth(player.getHealth() - present.getDamaging());
-
+    }
+    private void updatePotion() {
         Potion potion = map.getPotion(player.getX() + player.getWidth() / 2, player.getY() + 48);
 
         if (potion != null) {
@@ -49,7 +51,7 @@ public class PlayerController {
                         public void run() {
                             player.setSpeedX(player.getSpeedX() / 2);
                         }
-                    }, 500);
+                    }, 5000);
 
             }
             map.deletePotion(potion);
@@ -61,16 +63,21 @@ public class PlayerController {
         this.keyH = keyH;
         this.levels = levels;
         this.map = levels.getCurrentLevel();
+        timerC.schedule(new TimerTask() {
+            public void run() {
+                player.setImage();
+            }
+        }, 0, 200);
     }
 
     public void update() {
         moving();
     }
 
-    private boolean jumping = false;
-
     private void moving() {
-        updateMoveState();
+        updateCollision();
+        updatePotion();
+
         moveDown();
         if (keyH.upPressed && !jumping && isCollisionBottom) {
 //            player.setDirection("up");
@@ -92,21 +99,15 @@ public class PlayerController {
                 jumping = false;
             }
         }
-
-        timer--;
-
-        if (timer <= 0) {
-            player.setImage();
-            timer = 10;
-        }
     }
+
 
     private void moveNext() {
         if (!isCollisionMove) {
             int offset = (player.getX() - map.getWidth() / 2);
-            if (offset > 0 && player.getSpeedX() > 0 && map.getMaxOffsetX() != map.getOffsetX()) {
+            if (offset > 0 && player.getSpeedX() > 0 && map.getMaxOffsetX() >= map.getOffsetX() + player.getSpeedX()) {
                 map.setOffsetX(player.getSpeedX());
-            } else if (map.getOffsetX() != 0 && player.getSpeedX() < 0 && offset < 0) {
+            } else if ((map.getOffsetX() + player.getSpeedX()) >= 0 && player.getSpeedX() < 0 && offset < 0) {
                 map.setOffsetX(player.getSpeedX());
             } else if (player.getX() != map.getWidth() - 48 && player.getSpeedX() > 0 || player.getX() != 48 && player.getSpeedX() < 0) {
                 player.setX(player.getX() + player.getSpeedX());
