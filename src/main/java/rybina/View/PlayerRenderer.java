6805703f3fa6bac938249d04b530e728ed9b427d
rybinali <rybinaaaaa.a@gmail.com>
2024-06-06 13:37:29 +0200

    delete unnecessary

--- src/main/java/rybina/View/PlayerRenderer.java
+++ src/main/java/rybina/View/PlayerRenderer.java
@@ -13,7 +13,6 @@ public class PlayerRenderer {
 
     public void draw(Graphics2D g2) {
         g2.setColor(Color.white);
-        g2.drawOval(player.getX() + player.getRealWidth()/2, player.getY() - 47, 20, 20);
         g2.drawImage(player.getImage(), player.getX(), player.getY(), player.getWidth(), player.getHeight(), null);
     }
 }
